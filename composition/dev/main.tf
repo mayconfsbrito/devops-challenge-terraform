686d47e86ce650582b400527bdf99bc1f3959e78
Maycon <maycon.brito@bairesdev.com>
2022-02-06 17:53:12 -0300

    Renaming src functions

--- composition/dev/main.tf
+++ composition/dev/main.tf
@@ -9,10 +9,10 @@ module "aws_lambda" {
   //First lambda function (enqueue)
   first_function_name        = "enqueue_function"
   first_function_description = "Function to enqueue the request to the SQS queue"
-  first_function_source_path = "./src/first_function"
+  first_function_source_path = "./src/enqueue_function"
 
   //Second lambda function (dequeue)
   second_function_name        = "dequeue_function"
   second_function_description = "Function to enqueue the request to the SQS queue"
-  second_function_source_path = "./src/second_function"
+  second_function_source_path = "./src/dequeue_function"
 }
